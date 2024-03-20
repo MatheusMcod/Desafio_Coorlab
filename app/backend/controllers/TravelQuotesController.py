619773a361d7bb15b841375f0fe0da52fad226e6
MatheusMcod <matheus.moreiracosta300@gmail.com>
2024-03-20 18:08:17 -0300

    infrastructure_fix

---
+++ app/backend/controllers/TravelQuotesController.py
@@ -0,0 +1,10 @@
+from typing import List, Dict
+from fastapi import HTTPException
+from models.TravelQuotes import TravelQuotes
+import json
+
+class TravelQuotesController:
+	def __init__(self) -> None:
+		pass
+
+	
