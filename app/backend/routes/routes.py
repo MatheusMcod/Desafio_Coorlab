523e34899bc06024bd97096432d8066787673cb0
MatheusMcod <matheus.moreiracosta300@gmail.com>
2024-03-21 14:48:58 -0300

    search_trips

--- app/backend/routes/routes.py
+++ app/backend/routes/routes.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
 from controllers.TravelQuotesController import TravelQuotesController
+from typing import Any
 
 routerInstance = APIRouter()
 
 @routerInstance.get("/trips")
-def home():
-  response = TravelQuotesController().getAllTrips()
+async def searchAllTrips():
+  response = await TravelQuotesController().searchAllTrips()
   return response
 
+
+@routerInstance.get("/trips/{destination}")
+async def searchTrips(destination: str):
+  response = await TravelQuotesController().searchDestinationTrips(destination)
+  return response
 
