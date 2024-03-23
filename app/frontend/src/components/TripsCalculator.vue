a93f609a9adfefd711709ef6593eb0cf0724d6b5
MatheusMcod <matheus.moreiracosta300@gmail.com>
2024-03-23 17:06:03 -0300

    trips_calculator_module

---
+++ app/frontend/src/components/TripsCalculator.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="col-span-6 mt-20 w-3/4">
+
+		<div class="bg-gray-800 w-full h-16 rounded flex items-center pl-10 font-bold">
+			<h2 class="text-2xl text-white"><i class="fa-solid fa-truck-arrow-right mr-1"></i> Calculadora de Viagem</h2>
+		</div>
+
+		<div class="grid grid-cols-2 height">
+			<div class="bg-gray-300 w-3/4 h-5/6 mt-8 ml-8 flex flex-col items-center justify-center rounded">
+				<h2 class="mb-5 text-2xl font-semibold"><i class="fa-solid fa-hand-holding-dollar"></i> Calcule o Valor da Viagem</h2>
+				<form>
+					<label for="inputCitys" class="block text-black-700 font-semibold mb-2">Destino</label>
+					<input type="text" list="citys" id="inputCitys" name="inputCitys" placeholder="Selecione o Destino" class="w-80 form-input px-4 py-2 rounded-md focus:outline-none focus:ring">
+					<datalist id="citys"></datalist>
+					<label for="date" class="block text-black-700 font-semibold mb-2">Data</label>
+					<input type="date" name="date" id="date" class="w-80 form-input px-4 py-2 rounded-md focus:outline-none focus:ring">
+					<button type="submit" class="block bg-blue-500 hover:bg-blue-700 font-semibold rounded focus:outline-none focus:shadow-outline w-52 h-8 mt-10 mx-auto">Buscar</button>
+				</form>
+			</div>
+
+			<div class="flex flex-col items-center justify-center">
+				<h2 class="text-center text-xl font-bold">Nenhum dado selecionado...</h2>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	.height {
+		height: 40rem;
+	}
+</style>
--- app/frontend/src/views/HomeView.vue
+++ app/frontend/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TripsCalculator from '../components/TripsCalculator.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="grid h-screen grid-cols-6">
+		<nav class="col-span-1 bg-gray-800 text-white">
+			<div class="my-14 ml-5 w-52">
+				<img src="../assets/logo.png" alt="Logo da empresa.">
+			</div>
+			<ul class="text-center">
+        <li><button class="w-full h-10 hover:border hover:border-blue-500"><i class="fa-solid fa-calculator mr-2"></i> Calculadora de Viagem</button></li>
+      </ul>
+		</nav>
+		<main class="col-span-5">
+			<div class="w-full h-20 shadow-md shadow-gray-500 p-8"></div>
+			<div class="flex justify-center"><TripsCalculator /></div>
+		</main>
+  </div>
 </template>
